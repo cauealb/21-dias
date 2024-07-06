48ffd4b5c4dd3619c193928f5e725a233584b1e7
Cauê Alves <cauealvesb4@gmail.com>
2024-07-06 16:31:44 -0300

    Finalizado o dia 3

--- 3-dia/index.js
+++ 3-dia/index.js
@@ -1,4 +1,15 @@
-//Váriaveis (let and Const)
+//Entrada de Dados
 
+
+let nome = prompt('Insira seu nome: ');
+let idade = parseInt(prompt('Insira sua idade: '));
+const altura = Number(prompt('Insira sua Altura: '))
+let peso = Number(prompt('Insira seu peso: '))
+//Contas
+const imc = peso / (altura * altura)
+const nasceu = 2023 - idade
+
+
+console.log('Olá, ' + nome + ' você tem ' + idade + ' anos, nasceu em ' + nasceu + ' , tem ' + altura + ' de altura, pesa ' + peso + 'kg, seu IMC é ' + imc + ' Kg/m2')
 
 
