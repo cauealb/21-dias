e677402212f05e9ac3c0f868c6c1d73612645433
Cauê Alves <cauealvesb4@gmail.com>
2024-07-07 20:16:24 -0300

    commit

--- 4-dia/desafio-do-dia.js
+++ 4-dia/desafio-do-dia.js
@@ -13,16 +13,29 @@
 // }
 
 //Desafio número 2
-
+let temCarta = false
+let temCarro = false
 let nome = prompt('Digite seu nome:')
 let idade = parseInt(prompt('Digite sua idade: '))
 let carta = prompt('Tem carta de motorista:')
 let carro = prompt('Tem carro:')
 
-if(idade < 18 || carta === 'Não'){
+if(carta === 'Sim'){
+    temCarta = true
+} else{
+    temCarta = false
+}
+
+if(carro === 'Sim'){
+    temCarro = true
+}else{
+    temCarro = false
+}
+
+if(idade < 18 || !carta){
     console.log(nome, ', Você não pode dirigir!!!')
-}else if(idade >= 18 && carta === 'Sim' && carro === 'Não'){
+}else if(idade >= 18 && temCarta && !temCarro){
     console.log(nome, ', você pode dirigir mas não tem um carro')
-}else if(idade >= 18 && carta === 'Sim' && carro === 'Sim'){
+}else if(idade >= 18 && temCarta && temCarro){
     console.log(nome, ', você será o motorista!')
 }
