2bd882cab99ac95fd25e332e20e1b296d5231936
Cauê Alves <cauealvesb4@gmail.com>
2024-07-11 18:12:03 -0300

    commit

--- 7-dia/index.js
+++ 7-dia/index.js
@@ -1,4 +1,4 @@
-// DESAFIO 1
+//DESAFIO 1
 let contM = 0
 let contF = 0
 let contTotal = 0
@@ -13,7 +13,7 @@ while(true){
         break;
     }else{
         nota = parseInt(prompt('Qual sua nota:'))
-    contTotal += nota
+        contTotal += nota
 
     if(sexo == 'M'){
         contM++
@@ -39,3 +39,54 @@ alert(`Quantas mulheres tiveram nota acima de 7: ${contF7} nota(s).`)
 alert(`Qual a maior nota entre os homens: ${maior}.`)
 
 // DESAFIO 2
+
+let nome
+let cpf
+let saldo = 1000
+let valorPedido
+let opcao
+let maiorD = 0
+let total = 0
+let cont = 0
+
+do {
+    alert(`Saldo na sua conta: R$${saldo}`)
+
+    nome = prompt('Digite seu nome: ')
+    cpf = prompt('Digite seu CPF: ')
+    valorPedido = parseInt(prompt('Digite seu valor: '))
+
+    total += valorPedido
+    if(valorPedido > maiorD){
+        maiorD = valorPedido
+    }
+
+    opcao = prompt('Deseja saque ou depósito (S/D): ')
+    cont++
+
+    if(opcao === 'S'){
+        if(valorPedido > saldo){
+            alert('Não é permitido sacar um valor maior que o saldo!')
+
+        }else if(valorPedido < 0){
+            alert('Você não pode sacar um valor negativo!')
+
+        }else{
+            saldo -= valorPedido
+        }
+        
+    }else if(opcao === 'D'){
+        if(valorPedido < 0){
+            alert('Você não pode sacar um valor negativo!')
+        }else{
+            saldo += valorPedido
+        }
+    }
+           
+    opcao = Number(prompt('Deseja:\n1 - Continua\n2 - Parar: '))
+} while (opcao === 1);
+
+
+alert(`Saldo na sua conta: R$${saldo}`)
+alert(`Maior valor inserido: ${maiorD}`)
+alert(`Média dos valores inseridos: ${total / cont}`)
