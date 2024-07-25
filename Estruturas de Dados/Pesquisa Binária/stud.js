583ff4b4d91b205d682f08cd01521b3ae1b77d1f
Cauê Alves <cauealvesb4@gmail.com>
2024-07-25 16:56:42 -0300

    commit

---
+++ Estruturas de Dados/Pesquisa Binária/stud.js
@@ -0,0 +1,36 @@
+// Push
+// const array1 = [1, 2, 3, 4]
+
+// array.push(6)
+// console.log(array)
+// /////////////////////////////////////
+// Pop
+// const array2 = [1, 2, 3, 4]
+
+// array2.pop();
+// console.log(array2)
+
+// const elimited = array2.pop()
+// console.log(elimited)
+// ////////////////////////////////////
+// Shif and Unshifd
+
+// const array3 = [1, 2, 3, 4]
+
+// array3.shift();
+// console.log(array3)
+
+// array3.unshift(0);
+// console.log(array3)
+// ///////////////////////////////////
+
+// const array3 = [44, 51, 94, 85, 1, 31, 15]
+// let array = []
+
+// let findArray = array3.find(n => {if(n % 5 == 0) array.push(n)})
+// console.log(array)
+////////////////////////////////////////////////////////////////////
+
+
+
+
